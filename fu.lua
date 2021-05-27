f5ac5a77919a9005284e9039271af8dbc2a9331a
Christophe Delord <cdelord.fr>
2021-05-27 10:40:21 +0200

    zathura configuration

--- fu.lua
+++ fu.lua
@@ -1371,6 +1371,8 @@ function graphic_application_configuration()
         gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras
     ]]
 
+    script ".config/zathura/zathurarc"
+
 end
 
 -- }}}
