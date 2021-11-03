07dbb17a5c469d877b90e8abc9507d1472c463db
Christophe Delord <cdelord.fr>
2021-11-03 16:43:21 +0100

    fix nvim-cmp configuration

--- files/.config/nvim/lua/nvim-cmp.lua
+++ files/.config/nvim/lua/nvim-cmp.lua
@@ -12,17 +12,17 @@ cmp.setup {
         end,
     },
     mapping = {
-        ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ['<Down>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ['<Up>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ['<C-n>'] = cmp.mapping.select_next_item {behavior = cmp.SelectBehavior.Insert},
+        ['<C-p>'] = cmp.mapping.select_prev_item {behavior = cmp.SelectBehavior.Insert},
+        ['<Down>'] = cmp.mapping.select_next_item {behavior = cmp.SelectBehavior.Select},
+        ['<Up>'] = cmp.mapping.select_prev_item {behavior = cmp.SelectBehavior.Select},
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping {
-            i = cmp.mapping.confirm { select = true },
-            c = cmp.mapping.confirm { select = false },
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
         },
     },
     sources = cmp.config.sources({
