7f54257b8938d8fc1b80715efbb501f86ad1486e
Christophe Delord <cdelord.fr>
2022-06-01 18:28:19 +0200

    fix zls installation

--- fu.lua
+++ fu.lua
@@ -1733,7 +1733,7 @@ function zig_configuration()
         if version ~= curr_version then
             with_tmpdir(function(tmp)
                 sh("wget https://github.com/zigtools/zls/releases/download/"..version.."/x86_64-linux.tar.xz -O "..tmp.."/x86_64-linux.tar.xz")
-                sh("cd "..tmp.."; tar xJf x86_64-linux.tar.xz && mv bin/zls %(HOME)/.local/bin/zls")
+                sh("cd "..tmp.."; tar xJf x86_64-linux.tar.xz && mv bin/zls %(HOME)/.local/bin/zls && chmod +x %(HOME)/.local/bin/zls")
                 installed_packages.zls_version = version
             end)
         end
