82e81d795b916622980df06c6764c0a63cfcb592
Christophe Delord <cdelord.fr>
2023-03-13 09:32:39 +0100

    vscodium tentative

--- fu.lua
+++ fu.lua
@@ -2257,6 +2257,15 @@ function vscode_configuration()
         apt_install "apt-transport-https code"
     end
 
+    --[===[
+    if FEDORA and (force or upgrade or not installed "codium") then
+        sh("sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg &&"
+           ..[=[ sudo sh -c 'printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo' ]=]
+        )
+        dnf_install "codium"
+    end
+    --]===]
+
 end
 
 -- }}}
