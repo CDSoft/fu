99be05ac9cfc2c0399c356b611c535f0d7b30666
Christophe Delord <cdelord.fr>
2024-04-29 20:32:56 +0200

    fix docker installation

--- fu.lua
+++ fu.lua
@@ -2538,8 +2538,8 @@ function work_configuration()
         sh "sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo"
         dnf_install [[
             docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-            sh "sudo systemctl start docker || true"
         ]]
+        sh "sudo systemctl start docker || true"
     end
 
     if cfg.move_docker_to_home then
