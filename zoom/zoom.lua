2fe436bc7d9e840250539de142eee72c695dc981
Christophe Delord <cdelord.fr>
2024-11-28 10:00:25 +0100

    simplified zoom installation

--- zoom/zoom.lua
+++ zoom/zoom.lua
@@ -1,7 +1,5 @@
-if FORCE or not installed "zoom" then
-    fs.with_tmpdir(function(tmp)
-        run { "wget", "https://zoom.us/client/latest/zoom_x86_64.rpm", "-O", tmp/"zoom_x86_64.rpm" }
-        run { "sudo dnf install", tmp/"zoom_x86_64.rpm" }
-    end)
-    mime_default "Zoom.desktop"
-end
+dnf_install [[
+    https://zoom.us/client/latest/zoom_x86_64.rpm
+]]
+
+mime_default "Zoom.desktop"
