695a39036b033a9445c7a5290b0a25b5e848d892
Christophe Delord <cdelord.fr>
2024-11-29 18:00:12 +0100

    fix vifm configuration

--- filesystem/home/.config/vifm/plugins/FileExt/init.lua
+++ filesystem/home/.config/vifm/plugins/FileExt/init.lua
@@ -1,17 +1,30 @@
 -- FileExt shows two extensions when the file is an archive
 
+local max_len = 4
+
+local compressed = {
+    gz = true,
+    bz2 = true,
+    xz = true,
+    lz = true,
+}
+
 local added = vifm.addcolumntype {
     name = 'FileExt',
     handler = function(info)
         local e = info.entry
-        if e.isdir then return { text = "<dir>" } end
+        if e.isdir then return { text = "/" } end
         local name1 = vifm.fnamemodify(e.name, ':r')
         local ext1  = vifm.fnamemodify(e.name, ':e')
         local ext2  = vifm.fnamemodify(name1, ':e')
-        local ext =
-            #ext2>0 and ext1:match"%.?[gxl]z$"
-                and ext2.."."..ext1
-                or ext1
+        local ext
+        if #ext1 > max_len then
+            ext = ""
+        elseif #ext2>0 and compressed[ext1:lower()] then
+            ext = ext2.."."..ext1
+        else
+            ext = ext1
+        end
         if #ext >= info.width then
             ext = "…"..ext:sub(#ext-info.width+2)
         end
