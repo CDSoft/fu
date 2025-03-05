952761b54c9fd77df5a95160d3683802a4993415
Christophe Delord <CDSoft.github.io>
2025-03-05 21:15:35 +0100

    update zig

--- zig/zig.lua
+++ zig/zig.lua
@@ -1,31 +1,36 @@
-if FORCE or not installed "zig" or not installed "zls" then
+if UPDATE or not installed "zig" or not installed "zls" then
+
+    local ZIG_VERSION = "0.14.0" -- undefine for autodetection
 
     -- zig
     do
         local ZIG_URL = "https://ziglang.org/download/"
-        local index = download(ZIG_URL)
         local ZIG_ARCHIVE = nil
         local ZIG_DIR
-        index:gsub([[(https://ziglang%.org/download/[0-9.]+/(zig%-linux%-x86_64%-[0-9.]+)%.tar%.xz)]], function(url, name)
-            if not ZIG_ARCHIVE then
-                ZIG_ARCHIVE = url
-                ZIG_DIR = name
-            end
-        end)
-        assert(ZIG_ARCHIVE and ZIG_DIR, "Can not determine Zig version")
+        if not ZIG_VERSION then
+            local index = download(ZIG_URL)
+            index:gsub([[(https://ziglang%.org/download/[0-9.]+/(zig%-linux%-x86_64%-[0-9.]+)%.tar%.xz)]], function(url, name)
+                if not ZIG_ARCHIVE then
+                    ZIG_ARCHIVE = url
+                    ZIG_DIR = name
+                end
+            end)
+            assert(ZIG_ARCHIVE and ZIG_DIR, "Can not determine Zig version")
+            ZIG_VERSION = ZIG_ARCHIVE:match("/([%d%.]+)/")
+        else
+            ZIG_ARCHIVE = "https://ziglang.org/download/"..ZIG_VERSION.."/zig-linux-x86_64-"..ZIG_VERSION..".tar.xz"
+        end
         local curr_version = installed "zig" and read("zig version")
-        local version = ZIG_ARCHIVE:match("/([%d%.]+)/")
-        local ZIG_VERSION = version
 
-        if version ~= curr_version then
+        if ZIG_VERSION ~= curr_version then
             fs.mkdirs(HOME/".local/opt")
             download(ZIG_ARCHIVE, "~/.local/opt"/fs.basename(ZIG_ARCHIVE))
             run { "rm -rf", "~/.local/opt/zig"/ZIG_VERSION }
             run { "mkdir -p", "~/.local/opt/zig"/ZIG_VERSION }
             run { "tar xJf", "~/.local/opt"/fs.basename(ZIG_ARCHIVE), "-C", "~/.local/opt/zig"/ZIG_VERSION, "--strip-components 1" }
-            run { "ln -f -s", "~/.local/opt/zig"/ZIG_VERSION/"zig", "~/.local/bin/zig" }
             run { "rm -f", "~/.local/opt"/fs.basename(ZIG_ARCHIVE) }
         end
+        run { "ln -f -s", "~/.local/opt/zig"/ZIG_VERSION/"zig", "~/.local/bin/zig" }
     end
 
     -- zls
