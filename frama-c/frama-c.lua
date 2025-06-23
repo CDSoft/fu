c794043e90bf8e5cfcf60cbfa4cdd17fcf0d778e
Christophe Delord <cdelord.fr>
2025-06-23 08:47:58 +0200

    fix frama-c installation

--- frama-c/frama-c.lua
+++ frama-c/frama-c.lua
@@ -3,6 +3,15 @@ dnf_install [[
 ]]
 
 if FORCE or not installed "frama-c" then
-    run "opam install alt-ergo"
-    run "opam install frama-c"
+    local version = read "opam info frama-c" : lines()
+        : map(function(line)
+            local words = line:words()
+            if words[1] == "version" then return words[2] end
+        end)
+        : head() : split "%." : map(tonumber)
+        print("frama-c", F.show(version))
+    if version:head() >= 31 then
+        run "opam install alt-ergo"
+        run "opam install frama-c"
+    end
 end
