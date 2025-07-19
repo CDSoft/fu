5198559b52c57119f52f5ddff7fe314112de70a9
Christophe Delord <cdelord.fr>
2025-07-19 16:41:43 +0200

    avante

--- neovim/home/.config/nvim/init.lua
+++ neovim/home/.config/nvim/init.lua
@@ -602,6 +602,15 @@ use "yetone/avante.nvim" {
         -- add any opts here
         provider = ai_provider,
         providers = {
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-sonnet-4-20250514",
+                timeout = 30000, -- Timeout in milliseconds
+                extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 20480,
+                },
+            },
             mistral = {
                 __inherited_from = "openai",
                 api_key_name = "%(myconf.MISTRAL_API_KEY)",
@@ -615,8 +624,6 @@ use "yetone/avante.nvim" {
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
@@ -624,6 +631,8 @@ use "yetone/avante.nvim" {
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
         "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
         "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "stevearc/dressing.nvim", -- for input provider dressing
+        "folke/snacks.nvim", -- for input provider snacks
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         "zbirenbaum/copilot.lua", -- for providers='copilot'
         {
